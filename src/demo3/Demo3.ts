c986fce0569f35048d39c65d6e41223a1adaaf81
Alexander <slavshik@me.com>
2022-11-22 10:46:37 +0100

    fix: basic fire demo

--- src/demo3/Demo3.ts
+++ src/demo3/Demo3.ts
@@ -1,4 +1,5 @@
 import * as particles from "@pixi/particle-emitter";
+import {EmitterConfigV3} from "@pixi/particle-emitter";
 import {Container} from "pixi.js";
 import {AssetsHelper} from "../AssetsHelper";
 import {DemoView} from "../DemoView";
@@ -33,37 +34,40 @@ export class Demo3 extends DemoView {
         super.resize(width, height);
         this.emitterPivot.position.set(width * 0.5, height * 0.5);
     }
-    protected getEmitterConfig(): any {
+    protected getEmitterConfig(): EmitterConfigV3 {
         return {
-            lifetime: {min: 0.5, max: 0.5},
-            frequency: 0.008,
-            spawnChance: 1,
-            particlesPerWave: 1,
-            emitterLifetime: 0.31,
-            maxParticles: 1000,
-            pos: {x: 0, y: 0},
+            lifetime: {min: 0.1, max: 0.75},
+            frequency: 0.001,
+            emitterLifetime: 0,
+            maxParticles: 10,
             addAtBack: false,
+            pos: {x: 0, y: 0},
             behaviors: [
                 {
                     type: "alpha",
                     config: {
                         alpha: {
                             list: [
-                                {value: 0.8, time: 0},
-                                {value: 0.1, time: 1}
+                                {time: 0, value: 0.62},
+                                {time: 1, value: 0}
                             ]
                         }
                     }
+                },
+                {
+                    type: "moveSpeedStatic",
+                    config: {min: 50, max: 200}
                 },
                 {
                     type: "scale",
                     config: {
                         scale: {
                             list: [
-                                {value: 1, time: 0},
-                                {value: 0.3, time: 1}
+                                {time: 0, value: 0.25},
+                                {time: 1, value: 0.5}
                             ]
-                        }
+                        },
+                        minMult: 1
                     }
                 },
                 {
@@ -71,40 +75,103 @@ export class Demo3 extends DemoView {
                     config: {
                         color: {
                             list: [
-                                {value: "fb1010", time: 0},
-                                {value: "f5b830", time: 1}
+                                {time: 0, value: "fff191"},
+                                {time: 0.5, value: "ff9991"},
+                                {time: 1, value: "ff622c"}
                             ]
                         }
                     }
                 },
                 {
-                    type: "moveSpeed",
-                    config: {
-                        speed: {
-                            list: [
-                                {value: 200, time: 0},
-                                {value: 100, time: 1}
-                            ],
-                            isStepped: false
-                        }
-                    }
+                    type: "rotation",
+                    config: {accel: 0.1, minSpeed: 50, maxSpeed: 50, minStart: 265, maxStart: 275}
                 },
                 {
-                    type: "rotationStatic",
-                    config: {min: 0, max: 360}
+                    type: "textureRandom",
+                    config: {
+                        textures: ["assets/particle.png", "assets/Fire.png", "assets/Fire2.png"]
+                    }
                 },
                 {
                     type: "spawnShape",
                     config: {
                         type: "torus",
-                        data: {x: 0, y: 0, radius: 10}
+                        data: {x: 0, y: 0, radius: 10, innerRadius: 8, affectRotation: false}
                     }
-                },
-                {
-                    type: "textureSingle",
-                    config: {texture: AssetsHelper.getFlame()}
                 }
             ]
         };
+        //     return {
+        //         lifetime: {min: 0.5, max: 0.5},
+        //         frequency: 0.008,
+        //         spawnChance: 1,
+        //         particlesPerWave: 1,
+        //         emitterLifetime: 0.31,
+        //         maxParticles: 1000,
+        //         pos: {x: 0, y: 0},
+        //         addAtBack: false,
+        //         behaviors: [
+        //             {
+        //                 type: "alpha",
+        //                 config: {
+        //                     alpha: {
+        //                         list: [
+        //                             {value: 0.8, time: 0},
+        //                             {value: 0.1, time: 1}
+        //                         ]
+        //                     }
+        //                 }
+        //             },
+        //             {
+        //                 type: "scale",
+        //                 config: {
+        //                     scale: {
+        //                         list: [
+        //                             {value: 1, time: 0},
+        //                             {value: 0.3, time: 1}
+        //                         ]
+        //                     }
+        //                 }
+        //             },
+        //             {
+        //                 type: "color",
+        //                 config: {
+        //                     color: {
+        //                         list: [
+        //                             {value: "fb1010", time: 0},
+        //                             {value: "f5b830", time: 1}
+        //                         ]
+        //                     }
+        //                 }
+        //             },
+        //             {
+        //                 type: "moveSpeed",
+        //                 config: {
+        //                     speed: {
+        //                         list: [
+        //                             {value: 200, time: 0},
+        //                             {value: 100, time: 1}
+        //                         ],
+        //                         isStepped: false
+        //                     }
+        //                 }
+        //             },
+        //             {
+        //                 type: "rotationStatic",
+        //                 config: {min: 0, max: 360}
+        //             },
+        //             {
+        //                 type: "spawnShape",
+        //                 config: {
+        //                     type: "torus",
+        //                     data: {x: 0, y: 0, radius: 10}
+        //                 }
+        //             },
+        //             {
+        //                 type: "textureSingle",
+        //                 config: {texture: AssetsHelper.getFlame()}
+        //             }
+        //         ]
+        //     };
     }
 }
